08cd207c3080595f76fd4318a0c774d7da518925
Henrique Marques <allyson2308h@gmail.com>
2022-03-19 06:05:07 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login/Login.Screen';
+import Home from './src/pages/home/Home.Screen';
+import List from './src/pages/list/List.Screen';
+import Detail from './src/pages/list/detail/Detail.Screen';
+
+export default function App() {
+  return (
+    <Detail/>
+    // <Login />
+    // <Home />
+    // <List />
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
